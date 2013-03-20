ea40378ed948c9b8c9a182a22b65b6ef5d67beee
PapaOda <derek.duarte@gmail.com>
2013-03-20 02:42:19 -0700

    Create customer class and subclasses with customer types.

---
+++ SideWalkEmpire/src/com/hongkoto_gr3ymatter/sidewalkempire/Student.java
@@ -0,0 +1,12 @@
+package com.hongkoto_gr3ymatter.sidewalkempire;
+
+public class Student extends Customer {
+
+	public Student() {
+		preferences.put("name", "Student");
+		preferences.put("Donuts", 1.0);
+		preferences.put("Coffee Machine", 1.5);
+
+	}
+
+}
--- SideWalkEmpire/src/com/hongkoto_gr3ymatter/sidewalkempire/test.java
+++
@@ -1,5 +0,0 @@
-package com.hongkoto_gr3ymatter.sidewalkempire;
-
-public class test {
-//Testing eGit.
-}
