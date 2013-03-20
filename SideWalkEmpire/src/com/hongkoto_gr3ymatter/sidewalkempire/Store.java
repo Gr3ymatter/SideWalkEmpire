573cf1f04cc3717ccec0c7112d9ad9d904ea3620
PapaOda <derek.duarte@gmail.com>
2013-03-20 03:18:45 -0700

    create new store class

---
+++ SideWalkEmpire/src/com/hongkoto_gr3ymatter/sidewalkempire/Store.java
@@ -0,0 +1,43 @@
+package com.hongkoto_gr3ymatter.sidewalkempire;
+
+import java.util.HashMap;
+
+public class Store {
+	
+	HashMap stageConstraints, inventory, menuPrices, equipmentPrices, wholesalePrices;
+	
+	Store(int stageNumber){
+		// Constructor, build following HashMaps: stageConstraints, inventory, menuPrices, equipmentPrices, wholesalePrices
+		switch (stageNumber){
+		case 1: {
+			stageConstraints.put("Title", "The Avenues");
+			stageConstraints.put("Customers", ["Hippie", "Laborer", "Student");
+			stageConstraints.put("Equipment Available", "The Avenues");
+			stageConstraints.put("Title", "The Avenues");
+			stageConstraints.put("Title", "The Avenues");
+			stageConstraints.put("Title", "The Avenues");
+			stageConstraints.put("Title", "The Avenues");
+			
+		}
+		
+		
+		}			
+	}
+	
+	double determine_preference(HashMap preference){
+		//Method iterates through input Customer preference Hashmap, compares against store inventory, and outputs number greater than 0.0.
+		
+	}
+	
+	boolean convert_customer(double customerPreference){
+		// Method takes as input result of determine_preference() and sets to true if greater than 1 - customer enters store.
+		
+	}
+	
+	double make_purchase(){
+		// Method 
+		
+	}
+	
+
+}
