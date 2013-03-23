20545e4cd6702365e811d1bf9599dce20151d5a0
PapaOda <derek.duarte@gmail.com>
2013-03-23 04:13:41 -0700

    update Customer class with protected HashMap creation and public retrieval method.

--- SideWalkEmpire/src/com/hongkoto_gr3ymatter/sidewalkempire/Customer.java
+++ SideWalkEmpire/src/com/hongkoto_gr3ymatter/sidewalkempire/Customer.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class Customer {
 // This class will contain common character variables and methods & animation methods for characters 
-	HashMap<String,Double> preferences;
+	protected HashMap<String,Double> preferences;
 	HashMap<String,ArrayList<String>> comments; //("positive" key with array of possible comments, "negative" as well.  Implemented at individual character level.)
 	boolean converted; //when store convertCustomer method is run, store value here?
 	double purchase; // after make purchase is run, store value here?
@@ -14,9 +14,14 @@ public class Customer {
 	
 	Customer(){
 		// Only consistent aspect is name across customer constructors
-		preferences.put("name", null);
+		preferences = new HashMap<String,Double>();
 		
 	}
 	
-
+	public HashMap<String,Double> getPreferences(){
+		// get method for preferences HashMap
+		return preferences;
+		}
+	
+	
 }
